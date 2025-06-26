47e10c8f5581c063f58569febe2bd7ce562f10cf
Gaelle Timmermans <gaelle.timmermansr@interface3.be>
2025-06-26 08:42:40 +0200

    Exerice associations recurcives

---
+++ Association-Recurcive/Models/Personne.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Association_Recurcive.Models
+{
+    internal class Personne
+    {
+        private string _nom;
+        private List<Personne> ParentList { get; set; }
+        private List<Personne> DescenentList { get; set; }
+
+        public string Nom
+        {
+            get
+            {
+                return _nom;
+            }
+
+            set
+            {
+                _nom = value;
+            }
+        }
+        public Personne(string nom)
+        {
+            Nom = nom;
+        }
+
+        public void Afficher()
+        {
+            Console.WriteLine($"Je suis {Nom}");
+        }
+
+        public void AjouterParent(Personne p)
+        {
+            ParentList.Add(p);
+        }
+
+        public void AjouterEnfant(Personne p)
+        {
+            DescenentList.Add(p);
+        }
+
+    }
+}
---
+++ Association-Recurcive/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Association_Recurcive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
